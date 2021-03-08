381904d364fddee7541fcfd5e6c98fe348c2168b
Charles Shen <ssa19990208@foxmail.com>
2021-03-08 17:49:54 +0800

    rebuilt_with_Jingwei_Huangs_ransac

--- preprocess/instance_segmentation.py
+++ preprocess/instance_segmentation.py
@@ -3,136 +3,113 @@ import json
 import numpy as np
 from plyfile import *
 
-#文件夹名
-base_dir_name = 'E:\\dataset\\scannet\\scans'
-scene_name = 'scene0000_00'
-dir_name = os.path.join(base_dir_name, scene_name)
+type_use = ['window', 'kitchen counter', 'cabinet', \
+    'kitchen cabinets', 'floor', 'wall', 'door', 'ceiling', 'doorframe', 'shelf']
 
-instance_descriptor_name = os.path.join(dir_name, 'scene0000_00.aggregation.json') 
-ply_name = os.path.join(dir_name, 'scene0000_00_vh_clean_2.ply')
-part_dir = os.path.join(dir_name, 'instances')
-if not os.path.exists(part_dir):
-    os.mkdir(part_dir)
+'''
+描述：选取所需的模型，并且更新各种数据文件
+参数：根目录，场景id
+返回：无
+'''
+def select_shape(ROOT_FOLDER, scene_id):
+    ply_name = os.path.join(ROOT_FOLDER, scene_id, scene_id + '_vh_clean_2.labels.ply')
+    instance_descriptor_name = os.path.join(ROOT_FOLDER, scene_id, scene_id + '.aggregation.json') 
+    vertex_info_name = os.path.join(ROOT_FOLDER, scene_id, scene_id + '_vh_clean_2.0.010000.segs.json')
 
-with open(instance_descriptor_name, 'r', encoding = 'utf8')as fp:
-    instance_info = json.load(fp)
+    plydata = PlyData.read(ply_name)
+    vertexs = plydata['vertex']
+    faces = plydata['face']
 
-instances = instance_info['segGroups']
-vertex_info_name = os.path.join(dir_name, instance_info['segmentsFile'][8:])
+    with open(instance_descriptor_name, 'r', encoding = 'utf8')as fp:
+        instance_info = json.load(fp)
+    instances = instance_info['segGroups']
 
-plydata = PlyData.read(ply_name)
-vertexs = plydata['vertex']
-faces = plydata['face']
+    with open(vertex_info_name, 'r', encoding = 'utf8')as fp:
+        vertex_info_total = json.load(fp)
+        vertex_info = vertex_info_total['segIndices']
 
-with open(vertex_info_name, 'r', encoding = 'utf8')as fp:
-    vertex_info = json.load(fp)['segIndices']
+    vertex_segments_dict = {}
 
-print(len(vertex_info))
-
-vertex_segments_dict = {}
-
-for i in range(len(vertex_info)):
-    item = vertex_info[i]
-    if item in vertex_segments_dict:
-        vertex_segments_dict[item].append(i)
-    else: 
-        vertex_segments_dict[item] = []
-        vertex_segments_dict[item].append(i)
-
-
-type_use = ['window', 'table', 'kitchen counter', 'desk', 'cabinet', 'floor', 'wall', 'coffee table', 'door', 'ceiling', 'shelf', 'doorframe']
-index_total = [-1] * vertexs.count
-total_useful_points = 0
-show_vertexs = []
-
-point_count = 0
-face_count = 0
-point_list_total = {}
+    for i in range(len(vertex_info)):
+        item = vertex_info[i]
+        if item in vertex_segments_dict:
+            vertex_segments_dict[item].append(i)
+        else: 
+            vertex_segments_dict[item] = []
+            vertex_segments_dict[item].append(i)
 
 
-for item in instances:
-    vertex_group_list = item['segments']
-    item_id = item['objectId']
-    item_type = item['label']
-    if not item_type in type_use:
-        continue
-    point_list_total[item_id] = []
-    point_list = []
-    for group in vertex_group_list:
-        point_list += vertex_segments_dict[group]
-        point_count += len(vertex_segments_dict[group])
-    for i in range(len(point_list)):
-        index_total[point_list[i]] = total_useful_points
-        point_list_total[item_id].append(total_useful_points)
-        total_useful_points += 1
-        show_vertexs.append(vertexs[point_list[i]])
-print(point_count, face_count)
 
 
-show_faces = []
-for i in range(faces.count):
-    face = faces[i]
-    a = face[0][0]
-    b = face[0][1]
-    c = face[0][2]
-    if index_total[a] >= 0 and index_total[b] >= 0 and index_total[c] >= 0:
-        place_a = index_total[a]
-        place_b = index_total[b]
-        place_c = index_total[c]
-        place = ([place_a, place_b, place_c],)
-        show_faces.append(place)
-print(len(show_vertexs), len(show_faces))
- 
-show_vertexs = np.array(show_vertexs)
-show_faces = np.array(show_faces, dtype=[('vertex_indices', 'uint32', (3,))])
-new_vertex = PlyElement.describe(show_vertexs, 'vertex')
-new_face = PlyElement.describe(show_faces, 'face')
-show_ply = PlyData([new_vertex, new_face], text = True, byte_order = '<')
+    #原先的第i个点现在变成index_total个点了
+    index_total = [-1] * vertexs.count
+    total_useful_points = 0
+    show_vertexs = []
 
-save_name = 'scene0000_00_vh_clean_decreased.ply'
-save_place = os.path.join(dir_name, save_name)
-show_ply.write(save_place)
 
-vertexs = show_ply['vertex']
-faces = show_ply['face']
-for (item_id, point_list) in point_list_total.items():
-    part_name = os.path.join(part_dir, 'part_' + str(item_id) + '.off')
-    descript_name = os.path.join(part_dir, 'part_' + str(item_id) + '.txt')
-    off = open(part_name, 'w')
-    txt = open(descript_name, 'w')
+    for item in instances:
+        vertex_group_list = item['segments']
+        item_id = item['objectId']
+        item_type = item['label']
+        if not item_type in type_use:
+            continue
+        previous_point_list = []
+        for group in vertex_group_list:
+            previous_point_list += vertex_segments_dict[group]
+        for i in range(len(previous_point_list)):
+            index_total[previous_point_list[i]] = total_useful_points
+            total_useful_points += 1
+            show_vertexs.append(vertexs[previous_point_list[i]])
 
-    face_list = []
-    index = [-1] * vertexs.count
-    #print(vertexs.count)
-    #print(len(point_list))
-    for i in range(len(point_list)):
-        aa = point_list[i]
-        index[aa] = i
 
+    show_faces = []
     for i in range(faces.count):
         face = faces[i]
         a = face[0][0]
         b = face[0][1]
         c = face[0][2]
-        if index[a] >= 0 and index[b] >= 0 and index[c] >= 0:
-            place_a = index[a]
-            place_b = index[b]
-            place_c = index[c]
-            place = [place_a, place_b, place_c]
-            face_list.append(place)
-            face_count += 1
-            txt.write(str(i) + '\n')
+        if index_total[a] >= 0 and index_total[b] >= 0 and index_total[c] >= 0:
+            place_a = index_total[a]
+            place_b = index_total[b]
+            place_c = index_total[c]
+            place = ([place_a, place_b, place_c],)
+            show_faces.append(place)
+ 
+    show_vertexs = np.array(show_vertexs)
+    show_faces = np.array(show_faces, dtype=[('vertex_indices', 'uint32', (3,))])
+    new_vertex = PlyElement.describe(show_vertexs, 'vertex')
+    new_face = PlyElement.describe(show_faces, 'face')
+    show_ply = PlyData([new_vertex, new_face], text = True, byte_order = '<')
 
-    off.write('OFF\n')
-    off.write(str(len(point_list)) + ' ' + str(len(face_list)) + ' ' + '0\n')
-    for point in point_list:
-        x = vertexs[point]['x']
-        y = vertexs[point]['y']
-        z = vertexs[point]['z']
-        off.write(str(x) + ' ' + str(y) + ' ' + str(z) + '\n')
-    for face in face_list:
-        off.write('3 ' + str(face[0]) + ' ' + str(face[1]) + ' ' + str(face[2]) + '\n')
-    off.close()
-    txt.close()
-    print("written part", item_id, "total", len(point_list), "points", len(face_list), 'faces')
+    save_place = os.path.join(ROOT_FOLDER, scene_id, scene_id + '_vh_clean_2_selected.ply')
+    show_ply.write(save_place)
+    
+    new_segment_info = [-1] * len(show_vertexs)
+    segment_dict = {}
+    for i in range(len(vertex_info)):
+        if index_total[i] >= 0:
+            new_index = index_total[i]
+            old_segment = vertex_info[i]
+            new_segment = index_total[old_segment]
+            new_segment_info[new_index] = new_segment
+            segment_dict[old_segment] = new_segment
+
+    vertex_info_total['segIndices'] = new_segment_info
+
+    new_json_segment_name = os.path.join(ROOT_FOLDER, scene_id, scene_id + '_vh_clean_2.0.010000_selected.segs.json')
+    with open(new_json_segment_name, "w") as f:
+        json.dump(vertex_info_total, f)
+
+    new_instances = []
+    for item in instances:
+        if item['label'] in type_use:
+            for i in range(len(item['segments'])):
+                old_seg_id = item['segments'][i]
+                item['segments'][i] = segment_dict[old_seg_id]
+            new_instances.append(item)
+    
+    instance_info['segGroups'] = new_instances
+    new_json_instance_name = os.path.join(ROOT_FOLDER, scene_id, scene_id + '_selected.aggregation.json') 
+    with open(new_json_instance_name, "w") as f:
+        json.dump(instance_info, f)
     
