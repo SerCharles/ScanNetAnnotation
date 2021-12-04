1ce6c36a78e24d531f334fb85b66d09c2663ef56
CharlesShen <ssa19990208@foxmail.com>
2021-12-04 20:04:02 +0800

    floor plan visualize

--- background_annotation/floor_plan.py
+++ background_annotation/floor_plan.py
@@ -4,6 +4,7 @@ from math import *
 import numpy as np
 import argparse
 import glob
+import cv2
 
 def load_planes(base_dir, scene_id):
     """Load the ScanNet-Planes dataset
@@ -57,7 +58,7 @@ def load_planes(base_dir, scene_id):
     labels = np.array(labels, dtype='int32')
     return vertexs, faces, norms, labels
 
-def save_floorplan(save_place, points, edges, labels):
+def save_floorplan(save_place, points, edges, edge_labels, room_range):
     """Save the floorplan of the scene
         P: number of points in the floor plan
         E: number of edges in the floor plan
@@ -67,8 +68,9 @@ def save_floorplan(save_place, points, edges, labels):
         points [numpy float array], [P * 2]: [points in the 2d floor plan]
         edges [numpy int array], [E * 2]: [the id of the start and end points in the 2d floor plan]
         edge_labels [numpy int array], [E]: [the labels of each edges]
+        room_range [numpy float array], [2 * 3]: [the lower bound and upper bound of the room, (x, y, z)]
     """
-    save_dict = {'points': points.tolist(), 'edges': edges.tolist(), 'labels': labels.tolist()}
+    save_dict = {'points': points.tolist(), 'edges': edges.tolist(), 'labels': edge_labels.tolist(), 'range': room_range.tolist()}
     json_data = json.dumps(save_dict)
     f = open(save_place, 'w')
     f.write(json_data)
@@ -86,19 +88,69 @@ def load_floorplan(save_place):
         points [numpy float array], [P * 2]: [points in the 2d floor plan]
         edges [numpy int array], [E * 2]: [the id of the start and end points in the 2d floor plan]
         edge_labels [numpy int array], [E]: [the labels of each edges]
+        room_range [numpy float array], [2 * 3]: [the lower bound and upper bound of the room, (x, y, z)]
     """
     with open(save_place, 'r', encoding='utf8')as fp:
         data = json.load(fp)
     points = data['points']
     edges = data['edges']
     edge_labels = data['labels']
+    room_range = data['range']
     points = np.array(points, dtype=np.float32)
     edges = np.array(edges, dtype=np.int32)
     edge_labels = np.array(edge_labels, dtype=np.int32)
-    return points, edges, edge_labels
+    room_range = np.array(room_range, dtype=np.float32)
+    return points, edges, edge_labels, room_range
 
-def get_floorplan(vertexs, faces, norms, labels, threshold_norm=0.1, threshold_bottom=0.5):
+def visualize_floorplan(save_place, points, edges, edge_labels, room_range):
+    """Visualize the floorplan
+        min:(-2.006484, -3.001466)
+        max:(12.124748, 18.227722)
+        we set the pictures' range to be (-4, -6) to (16, 24), the picture size is 800 * 1200
+    
+        P: number of points in the floor plan
+        E: number of edges in the floor plan
+        
+    Args:
+        save_place [string]: [the saving place of the floorplan]
+        points [numpy float array], [P * 2]: [points in the 2d floor plan]
+        edges [numpy int array], [E * 2]: [the id of the start and end points in the 2d floor plan]
+        edge_labels [numpy int array], [E]: [the labels of each edges]
+        room_range [numpy float array], [2 * 3]: [the lower bound and upper bound of the room, (x, y, z)]
+    """
+    P = points.shape[0]
+    E = edges.shape[0]
+    min_x = -4.0 
+    min_y = -6.0
+    max_x = 16.0
+    max_y = 24.0
+    size_x = 800
+    size_y = 1200
+    image = np.zeros((size_y, size_x, 3), np.uint8)
+    point_color = (255, 255, 255)
+    thickness = 1 
+    line_type = 8
+    
+    for i in range(E):
+        start = int(edges[i, 0])
+        end = int(edges[i, 1])
+        start_x = float(points[start, 0])
+        start_y = float(points[start, 1])
+        end_x = float(points[end, 0])
+        end_y = float(points[end, 1])
+        place_start_x = int((start_x - min_x) / (max_x - min_x) * size_x)
+        place_start_y = int((start_y - min_y) / (max_y - min_y) * size_y)
+        place_end_x = int((end_x - min_x) / (max_x - min_x) * size_x)
+        place_end_y = int((end_y - min_y) / (max_y - min_y) * size_y)
+        point_start = (place_start_y, place_start_x)
+        point_end = (place_end_y, place_end_x)
+        cv2.line(image, point_start, point_end, point_color, thickness, line_type)
+
+    cv2.imwrite(save_place, image)
+
+def get_floorplan(vertexs, faces, norms, labels, threshold_norm=0.3):
     """Get the floorplan of the scene
+    
         V: number of vertexs of 3D model
         F: number of faces of 3D model
         P: number of points in the floor plan
@@ -110,16 +162,18 @@ def get_floorplan(vertexs, faces, norms, labels, threshold_norm=0.1, threshold_b
         norms [numpy float array], [F * 3]: [norms of each face]
         labels [numpy int array], [F]: [labels of each face]
         threshold_norm [float, 0.1 by default]: [the threshold that marks that the plane is a wall]
-        threshold_bottom [float, 0.2 by default]: [the threshold that marks that the point is at the bottom]
 
     Returns:
         points [numpy float array], [P * 2]: [points in the 2d floor plan]
         edges [numpy int array], [E * 2]: [the id of the start and end points in the 2d floor plan]
         edge_labels [numpy int array], [E]: [the labels of each edges]
+        room_range [numpy float array], [2 * 3]: [the lower bound and upper bound of the room, (x, y, z)]
     """
     V = vertexs.shape[0]
     F = faces.shape[0]
     wall_points = {}
+    
+    threshold_bottom = np.mean(vertexs[:, 2])
     
     #get the walls and their points
     for i in range(F):
@@ -151,7 +205,7 @@ def get_floorplan(vertexs, faces, norms, labels, threshold_norm=0.1, threshold_b
             else:
                 up_list.append(point)
         if len(up_list) != len(down_list):
-            return [], [], []
+            return [], [], [], []
 
         #allocate the point pairs
         for down in down_list:
@@ -213,16 +267,40 @@ def get_floorplan(vertexs, faces, norms, labels, threshold_norm=0.1, threshold_b
     points = []
     edges = []
     new_index_for_points = {}
+    total_down_z = 0.0
+    total_up_z = 0.0
+    num = 0
+    min_x = inf 
+    max_x = -inf 
+    min_y = inf 
+    max_y = -inf
     for down in useful_points:
         up = point_pair[down]
         down_x = float(vertexs[down, 0])
         down_y = float(vertexs[down, 1])
+        down_z = float(vertexs[down, 2])
         up_x = float(vertexs[up, 0])
         up_y = float(vertexs[up, 1])
+        up_z = float(vertexs[up, 2])
         new_x = (down_x + up_x) / 2.0
-        new_y = (down_y + up_y) / 2.0 
+        new_y = (down_y + up_y) / 2.0
+        total_down_z += down_z
+        total_up_z += up_z
+        num += 1 
+        if new_x < min_x:
+            min_x = new_x
+        if new_x > max_x:
+            max_x = new_x
+        if new_y < min_y:
+            min_y = new_y
+        if new_y > max_y:
+            max_y = new_y
         points.append([new_x, new_y])
         new_index_for_points[down] = len(points) - 1
+    if num <= 0:
+        return [], [], [], []
+    bottom_z = total_down_z / num 
+    top_z = total_up_z / num
     
     for i in range(len(walls_old)):
         wall_old = walls_old[i]
@@ -235,16 +313,16 @@ def get_floorplan(vertexs, faces, norms, labels, threshold_norm=0.1, threshold_b
     points = np.array(points, dtype=np.float32)
     edges = np.array(edges, dtype=np.int32)
     edge_labels = np.array(edge_labels, dtype=np.int32)
-
+    room_range = np.array([[min_x, min_y, bottom_z], [max_x, max_y, top_z]], dtype=np.float32)
     
-    return points, edges, edge_labels
+    return points, edges, edge_labels, room_range
     
 def main():
     """The main function
     """
     parser = argparse.ArgumentParser(description = '')
-    #parser.add_argument('--base_dir', default='/home1/shenguanlin/scannet_planes', type=str)
-    parser.add_argument('--base_dir', default='G:\\dataset\\scannet_planes_mine', type=str)
+    parser.add_argument('--base_dir', default='/home1/shenguanlin/scannet_planes', type=str)
+    #parser.add_argument('--base_dir', default='G:\\dataset\\scannet_planes_mine', type=str)
 
     args = parser.parse_args()
     full_name_list = glob.glob(os.path.join(args.base_dir, '*.ply'))
@@ -257,17 +335,21 @@ def main():
     scene_id_list.sort()
     right_num = 0
     wrong_num = 0
+
     for scene_id in scene_id_list:
-        save_place = os.path.join(args.base_dir, scene_id + '_floor_plan.json')
+        save_place_json = os.path.join(args.base_dir, scene_id + '_floor_plan.json')
+        save_place_picture = os.path.join(args.base_dir, scene_id + '_floor_plan.png')
         vertexs, faces, norms, labels = load_planes(args.base_dir, scene_id)
-        points, edges, edge_labels = get_floorplan(vertexs, faces, norms, labels)
+        points, edges, edge_labels, room_range = get_floorplan(vertexs, faces, norms, labels)
         if len(points) > 0:
-            save_floorplan(save_place, points, edges, edge_labels)
+            save_floorplan(save_place_json, points, edges, edge_labels, room_range)
+            visualize_floorplan(save_place_picture, points, edges, edge_labels, room_range)
             print('Rendered', scene_id)
             right_num += 1
         else:
             print('Error in rendering', scene_id)
             wrong_num += 1
     print(right_num, wrong_num)
+
 if __name__ == "__main__":
     main()
